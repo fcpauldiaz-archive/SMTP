e63e0f415b4188a9ce0955c46baac4f8e48a0438
Pablo Diaz <pablo5_diaz@hotmail.com>
2017-02-09 19:06:58 -0600

    create project

---
+++ src/smtp/SMTP.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smtp;
+
+/**
+ *
+ * @author SDX
+ */
+public class SMTP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        try
+            {
+               Class.forName("com.mysql.jdbc.Driver");
+            } catch (Exception e)
+            {
+               e.printStackTrace();
+            }
+    }
+    
+}
